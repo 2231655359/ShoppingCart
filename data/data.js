a7b45e6ce684a6867a530dbe70329f52356f51a9
神经蛙 <2231655359@qq.com>
2025-03-30 10:49:11 +0800

    购物车案例

---
+++ data/data.js
@@ -0,0 +1,26 @@
+const books=[
+  {
+    "bookName": "《算法导论》",
+    "publicationDate": "2006-9",
+    "price": 85,
+    "quantity": 1
+  },
+  {
+    "bookName": "《UNIX编程艺术》",
+    "publicationDate": "2006-2",
+    "price": 59,
+    "quantity": 1
+  },
+  {
+    "bookName": "《编程珠玑》",
+    "publicationDate": "2008-10",
+    "price": 39,
+    "quantity": 1
+  },
+  {
+    "bookName": "《代码大全》",
+    "publicationDate": "2006-3",
+    "price": 128,
+    "quantity": 1
+  }
+]
